8dfeda00508700542eb9f84ab8a7432e26cae8fb
mmm <margaret.m.matson@gmail.com>
2022-12-03 13:23:11 -0600

    fixed bug in require and reorganized/removed redundant tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,42 +1,24 @@
-const Employee = require('./lib/Employee');
+const Employee = require('../lib/Employee');
 
-//Test - creates employee object with name, id, and email properties
 describe('Employee', () => {
-    it('should create an employee object with name, id, and email properties', () => {
+    //Test - gets name - expect to be a string
+    it('getName() returns a string', () => {
         const employee = new Employee('name', 1, 'email');
-
-        expect(typeof employee.name).toBe('string');
-        expect(typeof employee.id).toBe('number');
-        expect(typeof employee.email).toBe('string');
-
+        expect(typeof employee.getName()).toBe('string');
     });
-});
-
-//Test - gets name - expect to be a string
-test('getName', () => {
-    const employee = new Employee('name', 1, 'email');
-    expect(typeof employee.name).toBe('string');
-});
-
-//Test - gets ID expect to be a number
-
-test('getID', () => {
-    const employee = new Employee('name', 1, 'email');
-    expect(typeof employee.id).toBe('number');
-});
-
-//Test - gets email - expect to be a string
-
-test('getEmail', () => {
-    const employee = new Employee('name', 1, 'email');
-    expect(typeof employee.email).toBe('string');
-});
-
-//Test - gets role returns employee - expect to be employee
-test('getRole', () => {
-    const employee = new Employee('name', 1, 'email');
-    const role = jest.fn(employee => employee.role);
-
-    role(employee);
-    expect(role).toHaveReturnedWith('Employee');
+    //Test - gets ID expect to be a number
+    it('getID() returns a number', () => {
+        const employee = new Employee('name', 1, 'email');
+        expect(typeof employee.getID()).toBe('number');
+    });
+    //Test - gets email - expect to be a string
+    it('getEmail() returns a string', () => {
+        const employee = new Employee('name', 1, 'email');
+        expect(typeof employee.getEmail()).toBe('string');
+    });
+    //Test - gets role returns employee - expect to be employee
+    it('getRole() returns "Employee"', () => {
+        const employee = new Employee('name', 1, 'email');
+        expect(employee.getRole()).toBe('Employee');
+    });
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,26 +1,14 @@
-const Engineer = require('./lib/Engineer');
+const Engineer = require('../lib/Engineer');
 
-//Test - creates engineer object with name, id, email, and github username properties
 describe('Engineer', () => {
-    it('should create an engineer object with name, id, email, and github properties', () => {
+    //Test - gets GitHub username - expect a string
+    it('getGithub() returns a string', () => {
         const engineer = new Engineer('name', 1, 'email', "github");
-
-        expect(typeof engineer.github).toBe('string');
-
+        expect(typeof engineer.getGithub()).toBe('string');
     });
-});
-
-//Test - gets GitHub username - expect a string
-test('getGithub', () => {
-    const engineer = new Engineer('name', 1, 'email', "github");
-    expect(typeof engineer.github).toBe('string');
-});
-
-//Test - gets role - overrides to return engineer - expect to be engineer
-test('getRole', () => {
-    const engineer = new Engineer('name', 1, 'email', "github");
-    const role = jest.fn(engineer => engineer.role);
-
-    role(engineer);
-    expect(role).toHaveReturnedWith('Engineer');
+    //Test - gets role - overrides to return engineer - expect to be engineer
+    it('getRole() returns "Engineer"', () => {
+        const engineer = new Engineer('name', 1, 'email', "github");
+        expect(engineer.getRole()).toBe('Engineer');
+    });
 });
